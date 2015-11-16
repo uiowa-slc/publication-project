3f40a6ec3d0f12caef29b81462a67db589d5a6ef
Dustin Quam <dustinquam@me.com>
2015-11-16 16:12:48 -0600

    added author notes

--- code/Author.php
+++ code/Author.php
@@ -1,24 +1,29 @@
 <?php
 class Author extends Contributor {
 
- private static $db = array(
-  
-    );
-    
- private static $has_one = array( 
+	private static $db = array(
 
- );
- 
- private static $many_many = array(
- 	'Articles' => 'Article'
- );   
+	);
 
-    
-   public function getCMSFields() {
-        $fields = parent::getCMSFields();
-        
-        
-        return $fields;
-    }
-    
+	private static $has_one = array(
+
+	);
+
+	private static $many_many = array(
+		'Articles' => 'Article',
+	);
+	private static $many_many_extraFields = array(
+		'Articles'     => array(
+			'ArticleNote' => 'HTMLText',
+		)
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Main', new HTMLEditorField(
+				'ManyMany[ArticleNote]',
+				'Author note related to article (specific to this article only)'
+			));
+		return $fields;
+	}
 }
