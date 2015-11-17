290c06ea1c9f89b9155a59dca1ed6bc0ea151219
Dustin Quam <dustinquam@me.com>
2015-11-17 12:47:50 -0600

    article category

--- code/Article.php
+++ code/Article.php
@@ -2,20 +2,22 @@
 class Article extends Page {
 	private static $db = array(
 		'FormattedTitle' => 'HTMLText',
-		'Citation'       => 'Text',
+		'Citation'       => 'HTMLText',
 	);
 
 	private static $has_one = array(
 		'Image'            => 'Image',
 		'PrintableArticle' => 'File',
+
+		'FeaturedTag' => 'ArticleTag',
 	);
 	private static $plural_name       = 'Articles';
 	private static $belongs_many_many = array(
 		'Authors' => 'Author',
 	);
 	private static $many_many = array(
-
-		'Tags' => 'ArticleTag',
+		'Categories' => 'ArticleCategory',
+		'Tags'       => 'ArticleTag',
 	);
 	private static $listing_page_class = 'Issue';
 	private static $show_in_sitetree   = false;
@@ -45,17 +47,23 @@ class Article extends Page {
 
 		$titleField = new HTMLEditorField('FormattedTitle', 'Formatted Article Title (if the title has formatting)');
 		$titleField->setRows(1);
+		$fields->addFieldToTab('Root.FormattedTitle', $titleField);
+
+		$fields->addFieldToTab('Root.Citation', new HTMLEditorField('Citation', 'Citation'));
 
 		$authorFieldConfig = GridFieldConfig_RelationEditor::create();
 		$authorGridField   = new GridField('Authors', 'Authors', $this->Authors(), $authorFieldConfig);
 		$fields->addFieldToTab('Root.Authors', $authorGridField);
 
-		$fields->addFieldToTab('Root.Main', $titleField, 'Content');
-		$fields->addFieldToTab('Root.Files', new UploadField('Image', 'Image (1920x1080 or 1280x720)'));
-		$fields->addFieldToTab('Root.Files', new UploadField('PrintableArticle', 'Download/Printable Version of the Article'));
+		$catField = TagField::create('Categories', 'Category', ArticleCategory::get(), $this->Tags())->setShouldLazyLoad(true);
+		$catField->setIsMultiple(false);
 
 		$tagField = TagField::create('Tags', 'Tags', ArticleTag::get(), $this->Tags())->setShouldLazyLoad(true);
 
+		$fields->addFieldToTab('Root.Files', new UploadField('PrintableArticle', 'Download/Printable Version of the Article'));
+		$fields->addFieldToTab('Root.Files', new UploadField('Image', 'Image (1920x1080 or 1280x720)'));
+
+		$fields->addFieldToTab('Root.Main', $catField, 'Content');
 		$fields->addFieldToTab('Root.Main', $tagField, 'Content');
 		return $fields;
 	}
@@ -67,6 +75,5 @@ class Article_Controller extends Page_Controller {
 	public function init() {
 		parent::init();
 	}
-
 
 }
