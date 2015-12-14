1a1b9bb00d3ac1ebd2ab45dddf987f19fa636d37
Dustin Quam <dustinquam@me.com>
2015-12-14 11:07:03 -0600

    minor article changes

--- code/Article.php
+++ code/Article.php
@@ -59,13 +59,22 @@ class Article extends Page {
 		$titleField->setRows(1);
 		$fields->addFieldToTab('Root.Main', $titleField);
 
+		//Author field - ArticleInfo tab
+		$authorFieldConfig = GridFieldConfig_RelationEditor::create();
+		$authorGridField   = new GridField('Authors', 'Authors', $this->Authors(), $authorFieldConfig);
+		$fields->addFieldToTab('Root.ArticleInfo', new LabelField('Search for an existing author (if they\'ve previously contributed to ILR) or add one below.'));
+		$fields->addFieldToTab('Root.ArticleInfo', $authorGridField);
+
+		//Citation field - ArticleInfo tab
+		$fields->addFieldToTab('Root.ArticleInfo', HTMLEditorField::create('Citation', 'Citation')->setRows(1));
+
 		//Article summary/expanded/downloadable text - Article Text tab
 		$fields->addFieldToTab('Root.ArticleText', new UploadField('PrintableArticle', 'Downloadable/printable version of the article'));
 		$fields->addFieldToTab('Root.ArticleText', new HTMLEditorField('Content', 'Article summary text or an entire short article'));
 		$fields->addFieldToTab('Root.ArticleText', HTMLEditorField::create('ExpandedText', 'Article full text (don\'t include the summary from the field above)')->setRows(40));
 		$tagField = TagField::create('Tags', 'Tags', ArticleTag::get(), $this->Tags())->setShouldLazyLoad(true);
 		$catField = DropdownField::create(
-			'ArticleTagID',
+			'FeaturedTagID',
 			'Featured tag (shows above the article\'s title)',
 			$this->Tags()->map('ID', 'Title')
 		)->setEmptyString('(Select a featured tag)');
@@ -75,20 +84,12 @@ class Article extends Page {
 		if ($this->Tags()->First()) {
 			$fields->addFieldToTab('Root.ArticleInfo', $catField);
 		} else {
-			$fields->addFieldToTab('Root.ArticleInfo', new LabelField('FeaturedTagLabel', ' Note: You must add tags and save this article before adding a featured tag.'));
+
 			$fields->addFieldToTab('Root.ArticleInfo', new ReadonlyField('FeaturedTagReadonly', 'Featured tag (shows above article title)'));
 
+			$fields->addFieldToTab('Root.ArticleInfo', new LabelField('FeaturedTagLabel', ' Note: You must add tags and save this article before adding a featured tag.'));
 		}
 		$fields->addFieldToTab('Root.ArticleInfo', $tagField);
-
-		//Author field - ArticleInfo tab
-		$authorFieldConfig = GridFieldConfig_RelationEditor::create();
-		$authorGridField   = new GridField('Authors', 'Authors', $this->Authors(), $authorFieldConfig);
-		$fields->addFieldToTab('Root.ArticleInfo', new LabelField('Search for an existing author (if they\'ve previously contributed to ILR) or add one below.'));
-		$fields->addFieldToTab('Root.ArticleInfo', $authorGridField);
-
-		//Citation field - ArticleInfo tab
-		$fields->addFieldToTab('Root.ArticleInfo', HTMLEditorField::create('Citation', 'Citation')->setRows(1));
 
 		//Footnotes field - Footnotes tab
 		$footnoteFieldConfig = GridFieldConfig_RelationEditor::create();
@@ -99,9 +100,7 @@ class Article extends Page {
 		$responseFieldConfig = GridFieldConfig_RelationEditor::create();
 		$responseFieldConfig->removeComponentsByType($responseFieldConfig->getComponentByType('GridFieldAddNewButton'));
 		$responseGridField = new GridField('Responses', 'Responses', $this->Responses(), $responseFieldConfig);
-		if ($this->ID == 0) {
-			$fields->addFieldToTab('Root.ArticleInfo', new LabelField('<strong>Note: You need to save a draft of this article before adding an author</strong><br />'));
-		}
+
 		$fields->addFieldToTab('Root.Responses', $responseGridField);
 
 		// Return fields
